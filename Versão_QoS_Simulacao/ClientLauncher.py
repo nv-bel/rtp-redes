0543302db5ae0c2cb8f02217df8f84fb36525451
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-05-13 17:56:28 -0300

    Remove argumentos de execução do cliente para facilitar demonstração

--- Versão_QoS_Simulacao/ClientLauncher.py
+++ Versão_QoS_Simulacao/ClientLauncher.py
@@ -1,19 +1,15 @@
-import sys
 from tkinter import Tk
 from Client import Client
 
 if __name__ == "__main__":
-	try:
-		serverAddr = sys.argv[1]
-		serverPort = sys.argv[2]
-		rtpPort = sys.argv[3]
-		fileName = sys.argv[4]	
-	except:
-		print("[Uso: ClientLauncher.py Nome_Servidor Porta_Servidor Porta_RTP Arquivo_Video]\n")	
-	
+	serverAddr = "127.0.0.1"
+	serverPort = 554
+	rtpPort = 5004
+	fileName = "movie.Mjpeg"
+
 	root = Tk()
-	
+
 	# Cria um novo cliente
 	app = Client(root, serverAddr, serverPort, rtpPort, fileName)
-	app.master.title("RTPClient")	
+	app.master.title("RTPClient")
 	root.mainloop()
